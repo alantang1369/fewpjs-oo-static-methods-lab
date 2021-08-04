1e22b62a2969d2f4b85067786b780c3087cd6ee6
alantang1369 <giabao_tang@yahoo.com>
2021-08-04 15:46:44 -0700

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 class Formatter {
   //add static methods here
+  static capitalize(string){
+    return string.split(' ').map( elt => elt.charAt(0).toUpperCase() + elt.slice(1)).join(' ')
+  }
+
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9-' ]/g,'');
+  }
+
+  static titleize(string){
+    const exludeWords = ['the','a','an','but','of','and','for','at','by','from'];
+    return string.split(' ').map((elt,i)=>{
+      if (i === 0 || !exludeWords.includes(elt) ){
+        return elt.charAt(0).toUpperCase() + elt.slice(1) 
+      }else {
+        return elt
+      }
+    }).join(' ')
+  }
 }
